281ce7812267c423070dff311cd7b52f4b01353b
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-09-15 08:49:10 -0500

    Link to file pr2392

--- pr2392/tsp.py
+++ pr2392/tsp.py
@@ -1,6 +1,8 @@
 from tsp_solver.greedy import solve_tsp
 
+
 def main():
+	"""pr2392 real file in https://github.com/czajkovsky/tsp/blob/master/in/pr2392.in"""
 	try:
 		f = open("pr2392.in", "r")
 		l = f.readline()
