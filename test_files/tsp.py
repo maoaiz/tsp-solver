fef40833f827bf8680b03009a4453f6c1cfed1cd
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-09-15 19:42:07 -0500

    Update tsp test

--- test_files/tsp.py
+++ test_files/tsp.py
@@ -6,27 +6,29 @@ def main():
     try:
         print "Leyendo archivo..."
         start_time = time.time()
-        f = open("data/pr2392a.in", "r")
+        f = open("data/pr2392.in", "r")
         l = f.readline()
         D = []
         for l in f.readlines():
             D.append(map(float, l.split()))
         print("--- %s seconds ---" % str(time.time() - start_time))
+        band = True
     except Exception, e:
         print e
-    print "Buscando la ruta mas corta para el viajero..."
-    try:
-        start_time = time.time()
-        path = solve_tsp( D )
-        print("---Ruta encontrada en %s seconds ---" % str(time.time() - start_time))
-    except Exception, e:
-        print "\n[ERROR]: %s\n" % e
-        path = None
-    if path:
-        print "\nsolution:", path
-    else:
-        print "Ocurrio un error"
-
+        band = False
+    if band:
+        print "Buscando la ruta mas corta para el viajero..."
+        try:
+            start_time = time.time()
+            path = solve_tsp( D )
+            print("---Ruta encontrada en %s seconds ---" % str(time.time() - start_time))
+        except Exception, e:
+            print "\n[ERROR]: %s\n" % e
+            path = None
+        if path:
+            print "\nsolution:", path
+        else:
+            print "Ocurrio un error"
 
 if __name__ == '__main__':
     main()
